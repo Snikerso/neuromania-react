bb3f3f2c74e312bb62f513e69aaf3f36b8e4f123
Snikerso <42245963+Snikerso@users.noreply.github.com>
2019-09-24 20:09:42 +0200

    Updates

---
+++ precache-manifest.f27736119d530cb1d827224316f658a8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "dd0b5dfbbe0afea8f253",
+    "url": "/neuromania-react/static/css/main.cdd63c40.chunk.css"
+  },
+  {
+    "revision": "dd0b5dfbbe0afea8f253",
+    "url": "/neuromania-react/static/js/main.dd0b5dfb.chunk.js"
+  },
+  {
+    "revision": "4e1a993a02a4c00e931c",
+    "url": "/neuromania-react/static/js/runtime~main.4e1a993a.js"
+  },
+  {
+    "revision": "8214d871d59ed10bde08",
+    "url": "/neuromania-react/static/js/2.8214d871.chunk.js"
+  },
+  {
+    "revision": "04eb77ae32546724692991bcb6d93d13",
+    "url": "/neuromania-react/static/media/Komorczakbeztla.04eb77ae.png"
+  },
+  {
+    "revision": "9c98b28852a47dd84c28c13484ba9b10",
+    "url": "/neuromania-react/static/media/n1x.9c98b288.jpg"
+  },
+  {
+    "revision": "651200d932ac4db41a97f1d4b8596c96",
+    "url": "/neuromania-react/static/media/n2x.651200d9.jpg"
+  },
+  {
+    "revision": "6b3f7081a719109a8a9c8eb006386545",
+    "url": "/neuromania-react/static/media/n3x.6b3f7081.png"
+  },
+  {
+    "revision": "7894da21f0c474b5deb6c0166c1df8ec",
+    "url": "/neuromania-react/static/media/n1x1.7894da21.jpg"
+  },
+  {
+    "revision": "18312537c4724b59ba62d05dfaa5861c",
+    "url": "/neuromania-react/static/media/n5x.18312537.jpg"
+  },
+  {
+    "revision": "63ac5c92e09cacdfb6cf57a15ffd57e1",
+    "url": "/neuromania-react/static/media/n6x.63ac5c92.jpg"
+  },
+  {
+    "revision": "b0af341f01eff9a9fd123136bbae7f44",
+    "url": "/neuromania-react/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/neuromania-react/precache-manifest.f27736119d530cb1d827224316f658a8.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/neuromania-react/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
